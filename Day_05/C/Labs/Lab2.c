0aba01007d0d23c5ebfe593a54721bf279348ebd
Amir Shetaia <amoor22009@gmail.com>
2021-08-10 01:29:12 +0200

    Added Day 05 of ITI embedded systems course

---
+++ Day_05/C/Labs/Lab2.c
@@ -0,0 +1,11 @@
+//Libraries including
+#include <stdio.h>
+
+void main()
+{
+    //Printing the sizes of different data types
+    printf("Size of char is %d bytes\n", sizeof(char));
+    printf("Size of int is %d bytes\n", sizeof(int));
+    printf("Size of float is %d bytes\n", sizeof(float));
+    printf("Size of double is %d bytes\n", sizeof(double));
+}
